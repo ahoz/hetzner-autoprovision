c23f094f5756c4fe98fed78b118a440e126839a2
Taha <ahoz@zdmr.de>
2020-06-20 00:29:55 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,51 @@
+# Provision VMs on Hetzer using Terraform and Ansible
+
+## Requirements
+
+- Terraform
+- Ansible
+- Hetzner
+- Hetzner Cloud Api Token
+
+## Configure variables
+
+Move *variables.tf.example* to *variables.tf*. All variables in _variables.tf_ needs to be set in order to provision vm's. 
+
+| Field                     | Description                                                  |
+| ------------------------- | ------------------------------------------------------------ |
+| hcloud_token              | Your Hetzner cloud project api token                         |
+| ssh_key                   | Your SSH key Name stored in your Hetzner cloud project       |
+| hcloud_ssh_key_local_path | SSH root user key file path on your client                   |
+| server_count              | Number of nodes you want to provision (max 100 nodes)        |
+| server_name               | Server name. (Used as prefix on multi-node provisions)       |
+| root_password             | Node root password                                           |
+| ssh_user_username         | New ssh user username                                        |
+| ssh_user_password         | New ssh user password                                        |
+| ssh_user_keyfile_path     | New ssh user keyfile path                                    |
+| ssh_port                  | New ssh port                                                 |
+| location                  | Hetzner vm location (https://docs.hetzner.cloud/#locations-get-all-locations) |
+| server_type               | Hetzner server type (https://docs.hetzner.cloud/#server-types) |
+| image                     | Hetzner image (only ubuntu images supported, https://docs.hetzner.cloud/#images) |
+
+## Custom settings
+
+### Firewall
+
+In order to set custom firewall rules *rules.v4* and *rules.v6* can be edited.
+
+### SSH
+
+In order to set custom SSH settings *sshd_config* can be edited.
+
+## Provisioning
+
+```bash
+terraform init
+terrform plan
+terraform apply
+```
+
+
+
+
+
---
+++ hetzner.tf
@@ -0,0 +1,79 @@
+provider "hcloud" {
+  token = "${var.hcloud_token}"
+}
+
+data "hcloud_ssh_key" "ssh_key" {
+    name = "${var.ssh_key}"
+}
+
+resource "hcloud_server" "web" {
+  count = "${var.server_count}"
+  image       = "${var.image}"
+  server_type = "${var.server_type}"
+  location = "${var.location}"
+  name = "${format("${var.server_name}-%03d", count.index + 1)}"
+  ssh_keys  = ["${data.hcloud_ssh_key.ssh_key.id}"]
+  connection {
+      user = "root"
+      host = "${hcloud_server.web[count.index].ipv4_address}"
+      private_key = "${file("${var.hcloud_ssh_key_local_path}")}"
+  }
+}
+
+resource "local_file" "fileappend" {
+  content =  "[hetzner-cloud]\n${join("\n", hcloud_server.web.*.ipv4_address)}"
+  filename = "${path.root}/ansible-hosts-config"
+}
+
+resource "null_resource" "installansible" {
+  count = "${var.server_count}"
+  connection {
+      user = "root"
+      host = "${hcloud_server.web[count.index].ipv4_address}"
+      private_key = "${file("${var.hcloud_ssh_key_local_path}")}"
+  }
+  provisioner "remote-exec" {
+   inline = [
+      "sudo apt update -y",
+      "sudo apt dist-update -y",
+      "sudo apt install software-properties-common -y",
+      "sudo apt-add-repository --yes --update ppa:ansible/ansible -y",
+      "sudo apt install ansible -y"
+    ]
+  }
+}
+
+resource "null_resource" "runansible" {
+  count = "1" 
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ansible-hosts-config ansible-dist.yml --extra-vars 'ssh_user_username=${var.ssh_user_username} ssh_user_password=${var.ssh_user_password} ssh_port=${var.ssh_port} ssh_user_keyfile_path=${var.ssh_user_keyfile_path} root_password=${var.root_password}'"
+  }
+  depends_on = [
+    null_resource.installansible,
+  ]
+}
+
+resource "null_resource" "restartnodes" {
+  count = "${var.server_count}"
+  connection {
+      user = "root"
+      host = "${hcloud_server.web[count.index].ipv4_address}"
+      private_key = "${file("${var.hcloud_ssh_key_local_path}")}"
+  }
+  provisioner "remote-exec" {
+   inline = [
+      "(sleep 20; echo hi) &"
+    ]
+  }
+  depends_on = [
+    null_resource.runansible,
+  ]
+}
+
+output "ip" {
+    value = "${join(",", hcloud_server.web.*.ipv4_address)}"
+}
+
+output "Done" {
+  value = "\n If ansible did not have printed any errors, then all nodes are provisioned successfully :-)\nDo not forget to delete variables.tf, since it contains sensitive information!"
+}
